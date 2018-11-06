c19564b22c1d10593d47b496c7c462d8fdf1ddc0
W1773ND <wilfriedwillend@gmail.com>
2018-11-06 12:29:56 +0100

    Enterprise description review

--- conf/urls.py
+++ conf/urls.py
@@ -24,6 +24,7 @@ urlpatterns = patterns(
     # url(r'^retail/', include('ikwen.partnership.urls', namespace='partnership')),
     # url(r'^theming/', include('ikwen.theming.urls', namespace='theming')),
     url(r'^kakocase/', include('ikwen_kakocase.kakocase.urls', namespace='kakocase')),
+    url(r'^rewarding/', include('ikwen.rewarding.urls', namespace='rewarding')),
     # url(r'^shavida/', include('ikwen_shavida.shavida.urls', namespace='shavida')),
     # url(r'^webnode/', include('ikwen_webnode.webnode.urls', namespace='webnode')),
     url(r'^', include('ikwen.core.urls', namespace='ikwen')),
