fe7fa7407d1ebb9c0c2997d5d17fd468c085f2a3
roddy <rmbogning@gmail.com>
2019-02-27 12:34:35 +0100

    bug fixing on dreamer template; and improve fixed and tested

--- tsunami/views.py
+++ tsunami/views.py
@@ -1,15 +1,21 @@
+import json
 import logging
 from threading import Thread
 
-
+from django.core.exceptions import MultipleObjectsReturned
 from django.core.mail import EmailMessage
-from django.views.generic.base import TemplateView
-
-from ikwen_kakocase.kakocase.models import TsunamiBundle
-from ikwen.core.utils import get_service_instance, get_mail_content
-
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
 from django.utils.translation import gettext as _
+from django.views.generic.base import TemplateView
+from ikwen_kakocase.kakocase.models import TsunamiBundle
 
+from ikwen.accesscontrol.models import Member, DEFAULT_GHOST_PWD
+from ikwen.core.utils import get_service_instance, get_mail_content
+from ikwen.revival.models import ProfileTag, MemberProfile
+
+TSUNAMI = 'tsunami'
 logger = logging.getLogger('ikwen')
 
 
@@ -26,6 +32,25 @@ class Home(TemplateView):
         service = get_service_instance()
         config = service.config
         visitor_email = request.GET.get('visitor_email')
+        if not  visitor_email:
+            response = {'error': 'No Email found'}
+            return HttpResponse(json.dumps(response), 'content-type: text/json')
+        try:
+            member = Member.objects.get(email=visitor_email)
+        except MultipleObjectsReturned:
+            member = Member.objects.filter(email=visitor_email)[0]
+        except Member.DoesNotExist:
+            username = visitor_email
+            member = Member.objects.create_user(username, DEFAULT_GHOST_PWD, email=visitor_email, is_ghost=True)
+        tag_fk_list = []
+        tag = TSUNAMI
+        tsunami_tag = ProfileTag.objects.get(slug=tag)
+        tag_fk_list.append(tsunami_tag.id)
+        member_profile = MemberProfile.objects.get(member=member)
+        member_profile.tag_fk_list.extend(tag_fk_list)
+        member_profile.save()
+
+
         try:
             subject = _("Do more with Tsunami !")
             html_content = get_mail_content(subject, template_name='tsunami/mails/first_contact.html',)
@@ -35,6 +60,8 @@ class Home(TemplateView):
             Thread(target=lambda m: m.send(), args=(msg,)).start()
         except:
             pass
+        next_url = reverse('tsunami:bundles')
+        return HttpResponseRedirect(next_url)
 
 
 class Maintenance(TemplateView):
