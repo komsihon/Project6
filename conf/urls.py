5fe05bc1a4d2500e70f76311a183f1c83a4db617
Kom Sihon <rsihon@gmail.com>
2019-07-31 23:14:15 +0100

    Foulassi URLs added to root URLconf

--- conf/urls.py
+++ conf/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 # from ikwen.flatpages.views import FlatPageView
 
-from website.views import Home, About, Webnode, Kakocase, Shavida, PinsView, Terms, Bundle
+from website.views import Home, About, Webnode, Kakocase, Shavida, PinsView, Terms, Bundle, Foulassi
 
 admin.autodiscover()
 
@@ -16,24 +16,25 @@ urlpatterns = patterns(
     url(r'^about$', About.as_view(), name='about'),
     url(r'^support_bundle$', Bundle.as_view(), name='support_bundle'),
     url(r'^terms$', Terms.as_view(), name='terms_and_conditions'),
-    url(r'^webnode$', Webnode.as_view(), name='webnode'),
-    url(r'^kakocase$', Kakocase.as_view(), name='kakocase'),
-    url(r'^shavida$', Shavida.as_view(), name='shavida'),
-    url(r'^pinsview$', PinsView.as_view(), name='pinsview'),
+    url(r'^webnode/$', Webnode.as_view(), name='webnode'),
+    url(r'^kakocase/$', Kakocase.as_view(), name='kakocase'),
+    url(r'^shavida/$', Shavida.as_view(), name='shavida'),
+    url(r'^pinsview/$', PinsView.as_view(), name='pinsview'),
     url(r'^tsunami/', include('tsunami.urls', namespace='tsunami')),
     url(r'^laakam/', include(admin.site.urls)),
     url(r'^billing/', include('ikwen.billing.urls', namespace='billing')),
     url(r'^theming/', include('ikwen.theming.urls', namespace='theming')),
-    # url(r'^cashout/', include('ikwen.cashout.urls', namespace='cashout')),
-    # url(r'^retail/', include('ikwen.partnership.urls', namespace='partnership')),
     url(r'^theming/', include('ikwen.theming.urls', namespace='theming')),
     url(r'^kakocase/', include('ikwen_kakocase.kakocase.urls', namespace='kakocase')),
     url(r'^kako/', include('ikwen_kakocase.kako.urls', namespace='kako')),
     url(r'^rewarding/', include('ikwen.rewarding.urls', namespace='rewarding')),
-    # url(r'^shavida/', include('ikwen_shavida.shavida.urls', namespace='shavida')),
     url(r'^webnode/', include('ikwen_webnode.webnode.urls', namespace='webnode')),
     url(r'^rewarding/', include('ikwen.rewarding.urls', namespace='rewarding')),
-    # url(r'^cci/', include('ikwen_kakocase.cci.urls', namespace='cci')),
+
+    # Foulassi URLs
+    url(r'^foulassi/school/', include('ikwen_foulassi.school.urls', namespace='school')),
+    url(r'^foulassi/', include('ikwen_foulassi.foulassi.urls', namespace='foulassi')),
+
     url(r'^', include('ikwen.core.urls', namespace='ikwen')),
     # url(r'^page/(?P<url>[-\w]+)/$', FlatPageView.as_view(), name='flatpage'),
 )
